dcb90c7a54b8b9984ba3746e08b3d2f328619aac
Michel Perez <michel.perez8402@gmail.com>
2016-05-09 10:11:45 -0600

    Adds `ProcessMedia` action constant

--- src/constants.php
+++ src/constants.php
@@ -3,6 +3,7 @@
 // Media Actions
 define('ACTION_MEDIA_ADD_MEDIA', 'AddMedia');
 define('ACTION_MEDIA_ADD_MEDIA_BENCHMARK', 'AddMediaBenchmark');
+define('ACTION_MEDIA_PROCESS_MEDIA', 'ProcessMedia');
 define('ACTION_MEDIA_GET_STATUS', 'GetStatus');
 define('ACTION_MEDIA_GET_MEDIA_INFO', 'GetMediaInfo');
 define('ACTION_MEDIA_GET_MEDIA_INFO_EX', 'GetMediaInfoEx');
