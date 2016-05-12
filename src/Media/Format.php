49a08855f9e97ebabde9374e39cf2c7be310e49e
Michel Perez <michel.perez8402@gmail.com>
2016-05-11 18:03:29 -0600

    Implements format helpers

--- src/Media/Format.php
+++ src/Media/Format.php
@@ -32,6 +32,8 @@ class Format extends \MobileRider\Encoding\Generics\DataItem
     public function setOptions($options)
     {
         $this->options = array_merge($this->options, $options);
+
+        return $this;
     }
 
     public function clearOptions()
@@ -42,6 +44,11 @@ class Format extends \MobileRider\Encoding\Generics\DataItem
     public function getOptions()
     {
         return $this->options;
+    }
+
+    public function getOption($name)
+    {
+        return array_key_exists($name, $this->options) ? $this->options[$name] : null;
     }
 
     public function setDestinationStatus($destination, $status = '')
@@ -57,5 +64,74 @@ class Format extends \MobileRider\Encoding\Generics\DataItem
     public function hasMultipleDestinations()
     {
         return count($this->destinations) > 1;
+    }
+
+    public function useVideoCodec($codec, $bitrate, array $settings = [])
+    {
+        return $this->setOptions(array(
+            'video_codec' => $codec,
+            'bitrate' => $bitrate,
+            'video_codec_parameters' => $settings
+        ));
+    }
+
+    public function useAudioCodec($codec, $bitrate, $channels = 2, $sampleRate = 44100, $volume = 100)
+    {
+        return $this->setOptions(array(
+            'audio_codec' => $codec,
+            'audio_bitrate' => $bitrate,
+            'audio_channels' => $channels,
+            'audio_sample_rate' => $sampleRate,
+            'audio_volume' => $volume
+        ));
+    }
+
+    public function withinBitrates($min, $max)
+    {
+        return $this->setOptions(array(
+            'minrate' => $min,
+            'maxrate' => $max
+        ));
+    }
+
+    public function segment($start, $duration = null)
+    {
+        return $this->setOptions(array(
+            'minrate' => $min,
+            'maxrate' => $max
+        ));
+    }
+
+    public function addMultipleOption($name, array $value)
+    {
+        if ($this->hasOption($name)) {
+            $value = array_merge($this->getOption($name), $value);
+        }
+
+        return $this->setOptions(array(
+            $name => implode(',', $value)
+        ));
+    }
+
+    public function addBitrates(array $value)
+    {
+        return $this->setMultipleOption('bitrates', $value);
+    }
+
+    public function addFramerates(array $value)
+    {
+        return $this->setMultipleOption('framerates', $value);
+    }
+
+    public function addKeyframes(array $value)
+    {
+        return $this->setMultipleOption('keyframes', $value);
+    }
+
+    public function setSize($width, $height)
+    {
+        return $this->setOptions(array(
+            'size' => $width . 'x' . $height
+        ));
     }
 }
